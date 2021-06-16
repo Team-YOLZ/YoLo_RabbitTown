6a25a8ede80c93748bae5ba1388662aa7fe6f4d5
Shin-Yong-Jae <sms3284@naver.com>
2021-06-16 20:13:22 +0900

    PederationLogin-Apple

--- Function/PlayerCtrl.cs
+++ Function/PlayerCtrl.cs
@@ -22,8 +22,7 @@ public class PlayerCtrl : MonoBehaviour
     }
     private static PlayerCtrl instance;
     public float _speed = 10f;
-    public GameObject obj_player;
-    public GameObject obj_playerCameraArm; //cinemachine Fallow Target
+    public GameObject obj_player;        
     Animator player_anim;
     Rigidbody rb;
     public GameObject[] enemy1;
@@ -45,21 +44,5 @@ public class PlayerCtrl : MonoBehaviour
                 enemy1[i].GetComponent<EnemyCtrl>().TakeDamage(5);
             }
         }
-    }
-
-    void FixedUpdate()
-    {
-        if (Application.platform == RuntimePlatform.WindowsEditor) //Editor에서 테스트하기 편하도록
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            player_anim.SetFloat("Move", Mathf.Abs(moveHorizontal) + Mathf.Abs(moveVertical)); //애니메이션 SetTrigger -> Float으로 변경
-
-            rb.velocity = new Vector3(moveHorizontal * _speed, 0, moveVertical * _speed);            
-        }
-
-        //CameraArm의 경우 Player보다 살짝 위로 두고 Player와 Rotation은 공유하지 않음
-        obj_playerCameraArm.transform.position = new Vector3(player_anim.transform.position.x, player_anim.transform.position.y + 3.5f, player_anim.transform.position.z);
     }
 }
